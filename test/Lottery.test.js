685c5eab657224393d32e949b8c27a50d76cfcc1
Vishal Sharma <vsh1819@gmail.com>
2018-03-21 17:29:37 +0000

    test passing to check we can send money to winner which also resets player array

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -84,5 +84,21 @@ describe('Lottery Contract', () => {
     }
   })
 
+  it('sends money to winner and resets player array', async () => {
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      value: web3.utils.toWei('2', 'ether')
+    })
+
+    const initialBalance = await web3.eth.getBalance(accounts[0])
+    await lottery.methods.pickWinner().send({ from: accounts[0]})
+    const finalBalance = await web3.eth.getBalance(accounts[0])
+    const difference = finalBalance - initialBalance
+    
+    assert(difference > web3.utils.toWei('1.8', 'ether'))
+  })
+
+
+
 
 })
