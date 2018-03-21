ba33f8e1d8452bc3cb7eb56d493ebfa2fb876605
Vishal Sharma <vsh1819@gmail.com>
2018-03-21 17:21:41 +0000

    test passing that only manager can pick winner

--- test/Lottery.test.js
+++ test/Lottery.test.js
@@ -73,9 +73,16 @@ describe('Lottery Contract', () => {
   }
   })
 
-
-
-
+  it('only manager can call pickWinner', async () => {
+    try {
+      await lottery.methods.pickWinner().send({
+        from: accounts[1]
+      })
+      assert(false)
+    } catch (err) {
+      assert(err)
+    }
+  })
 
 
 })
